aa50877a702185c212b74d88e13dbe0006396bbc
Rafael Marques <rfmapp@gmail.com>
2015-11-24 11:00:38 -0300

    Alterado o arquivo 'about.blade.php' para fazer uso do template de layout.

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,12 +1,7 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Sobre esse Sistema</title>
-</head>
-<body>
+@extends('main')
+
+@section('content')
     <h1>Esse é um sistema de aprendizado.</h1>
     <br>
     <h3>Criado por {{ $first_name }} {{ $last_name }}.</h3>
-</body>
-</html>
+@endsection
