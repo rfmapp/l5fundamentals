3a164238f791bc6f5fa5be4f73062064756ebddb
Rafael Marques <rfmapp@gmail.com>
2015-11-24 11:07:48 -0300

    Alterado o arquivo 'PagesController', adicionando a funçao para lidar com as requisiçoes a pagina de contato.

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -9,13 +9,19 @@ use App\Http\Controllers\Controller;
 
 class PagesController extends Controller
 {
-    // Funçao que controla a exibiçao da pagina "sobre".
+    // Função que controla a exibição da página "sobre".
     public function about()
     {
-        // Passagem de variaveis (dados) na renderizaçao da pagina.
+        // Passagem de variáveis (dados) na renderização da página.
         $first_name = 'Rafael';
         $last_name = 'Marques';
-        // Chamada da view da pagina "sobre" junto com os dados acima (first_name e last_name).
+        // Chamada da view da página "sobre" junto com os dados acima (first_name e last_name).
         return view('pages.about', compact('first_name', 'last_name'));
+    }
+
+    // Função de controla a exibição da página "contato".
+    public function contact()
+    {
+        return view('pages.contact');
     }
 }
